4dcc3092ca792ec245d5ca8c7dae0c6ba82debc6
Vadim Yastrebov <5952749+CFD2@users.noreply.github.com>
2021-01-12 08:50:49 -0500

    remove Hyperflash from ccx

--- boards/arm/imxrt/ccx/src/imxrt_flexspi_nor_flash.c
+++ boards/arm/imxrt/ccx/src/imxrt_flexspi_nor_flash.c
@@ -44,45 +44,7 @@
  * Public Data
  ****************************************************************************/
 
-#if defined (CONFIG_CCX_HYPER_FLASH)
-__attribute__((section(".boot_hdr.conf")))
-const struct flexspi_nor_config_s g_flash_config =
-{
-  .mem_config               =
-  {
-    .tag                    = FLEXSPI_CFG_BLK_TAG,
-    .version                = FLEXSPI_CFG_BLK_VERSION,
-    .read_sample_clksrc     = FLASH_READ_SAMPLE_CLK_EXTERNALINPUT_FROM_DQSPAD,
-    .cs_hold_time           = 3u,
-    .cs_setup_time          = 3u,
-    .column_address_width   = 3u,
-
-    /* Enable DDR mode, Word addassable, Safe configuration, Differential clock */
-
-    .controller_misc_option = (1u << FLEXSPIMISC_OFFSET_DDR_MODE_EN) |
-                              (1u << FLEXSPIMISC_OFFSET_WORD_ADDRESSABLE_EN) |
-                              (1u << FLEXSPIMISC_OFFSET_SAFECONFIG_FREQ_EN) |
-                              (1u << FLEXSPIMISC_OFFSET_DIFFCLKEN),
-    .sflash_pad_type        = SERIAL_FLASH_8PADS,
-    .serial_clk_freq        = FLEXSPI_SERIAL_CLKFREQ_133MHz,
-    .sflash_a1size          = 64u * 1024u * 1024u,
-    .data_valid_time        = {16u, 16u},
-    .lookup_table           =
-    {
-      /* Read LUTs */
-
-      FLEXSPI_LUT_SEQ(CMD_DDR, FLEXSPI_8PAD, 0xa0, RADDR_DDR, FLEXSPI_8PAD, 0x18),
-      FLEXSPI_LUT_SEQ(CADDR_DDR, FLEXSPI_8PAD, 0x10, DUMMY_DDR, FLEXSPI_8PAD, 0x06),
-      FLEXSPI_LUT_SEQ(READ_DDR, FLEXSPI_8PAD, 0x04, STOP, FLEXSPI_1PAD, 0x0),
-    },
-  },
-  .page_size                = 512u,
-  .sector_size              = 256u * 1024u,
-  .blocksize                = 256u * 1024u,
-  .is_uniform_blocksize     = 1,
-};
-
-#elif defined (CONFIG_CCX_QSPI_FLASH)
+#if defined (CONFIG_CCX_QSPI_FLASH)
 __attribute__((section(".boot_hdr.conf")))
 const struct flexspi_nor_config_s g_flash_config =
 {
