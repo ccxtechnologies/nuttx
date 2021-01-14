0b946b0c0259d62a1d45c2e007b55bd31a67efac
Vadim Yastrebov <5952749+CFD2@users.noreply.github.com>
2021-01-14 11:50:00 -0500

    ccx: cleanup imxrt_bringup.c

--- boards/arm/imxrt/ccx/src/imxrt_bringup.c
+++ boards/arm/imxrt/ccx/src/imxrt_bringup.c
@@ -45,23 +45,7 @@
 #include <debug.h>
 
 #include <syslog.h>
-#include <nuttx/i2c/i2c_master.h>
-#include <imxrt_lpi2c.h>
-#include <imxrt_lpspi.h>
 
-#ifdef CONFIG_IMXRT_USDHC
-#  include "imxrt_usdhc.h"
-#endif
-
-#ifdef CONFIG_USBMONITOR
-#  include <nuttx/usb/usbmonitor.h>
-#endif
-
-#ifdef CONFIG_PL2303
-#  include <nuttx/usb/pl2303.h>
-#endif
-
-#include "imxrt_enet.h"
 #include "ccx.h"
 
 #include <arch/board/board.h>  /* Must always be included last */
@@ -70,74 +54,9 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
-/* Checking needed by MMC/SDCard */
-
-#ifdef CONFIG_NSH_MMCSDMINOR
-#  define MMCSD_MINOR CONFIG_NSH_MMCSDMINOR
-#else
-#  define MMCSD_MINOR 0
-#endif
-
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
-
-#if defined(CONFIG_I2C_DRIVER) && defined(CONFIG_IMXRT_LPI2C)
-static void imxrt_i2c_register(int bus)
-{
-  FAR struct i2c_master_s *i2c;
-  int ret;
-
-  i2c = imxrt_i2cbus_initialize(bus);
-  if (i2c == NULL)
-    {
-      serr("ERROR: Failed to get I2C%d interface\n", bus);
-    }
-  else
-    {
-      ret = i2c_register(i2c, bus);
-      if (ret < 0)
-        {
-          serr("ERROR: Failed to register I2C%d driver: %d\n", bus, ret);
-          imxrt_i2cbus_uninitialize(i2c);
-        }
-    }
-}
-#endif
-
-#ifdef CONFIG_IMXRT_USDHC
-static int nsh_sdmmc_initialize(void)
-{
-  struct sdio_dev_s *sdmmc;
-  int ret = 0;
-
-  /* Get an instance of the SDIO interface */
-
-  sdmmc = imxrt_usdhc_initialize(0);
-  if (!sdmmc)
-    {
-      syslog(LOG_ERR, "ERROR: Failed to initialize SD/MMC\n");
-    }
-  else
-    {
-      /* Bind the SDIO interface to the MMC/SD driver */
-
-      ret = mmcsd_slotinitialize(0, sdmmc);
-      if (ret != OK)
-        {
-          syslog(LOG_ERR,
-                 "ERROR: Failed to bind SDIO to the MMC/SD driver: %d\n",
-                 ret);
-        }
-
-      imxrt_usdhc_set_sdio_card_isr(sdmmc, NULL, NULL);
-    }
-
-  return OK;
-}
-#else
-#  define nsh_sdmmc_initialize() (OK)
-#endif
 
 /****************************************************************************
  * Public Functions
@@ -168,75 +87,6 @@ int imxrt_bringup(void)
     {
       syslog(LOG_ERR, "ERROR: Failed to mount procfs at /proc: %d\n", ret);
     }
-#endif
-
-#if defined(CONFIG_I2C_DRIVER) && defined(CONFIG_IMXRT_LPI2C1)
-  imxrt_i2c_register(1);
-#endif
-
-#ifdef CONFIG_IMXRT_USDHC
-  /* Initialize SDHC-base MMC/SD card support */
-
-  nsh_sdmmc_initialize();
-#endif
-
-#ifdef CONFIG_MMCSD_SPI
-  /* Initialize SPI-based MMC/SD card support */
-
-  imxrt_spidev_initialize();
-
-  ret = imxrt_mmcsd_spi_initialize(MMCSD_MINOR);
-  if (ret < 0)
-    {
-      syslog(LOG_ERR, "ERROR: Failed to initialize SD slot %d: %d\n", ret);
-    }
-#endif
-
-#if defined(CONFIG_IMXRT_USBOTG) || defined(CONFIG_USBHOST)
-  ret = imxrt_usbhost_initialize();
-  if (ret != OK)
-    {
-      syslog(LOG_ERR, "ERROR: Failed to start USB host services: %d\n", ret);
-      return ret;
-    }
-#endif
-
-#ifdef CONFIG_USBMONITOR
-  /* Start the USB Monitor */
-
-  ret = usbmonitor_start();
-  if (ret != OK)
-    {
-      syslog(LOG_ERR, "ERROR: Failed to start USB monitor: %d\n", ret);
-    }
-#endif
-
-#if defined(CONFIG_IMXRT_ENET) && defined(CONFIG_NETDEV_LATEINIT)
-  ret = imxrt_netinitialize(0);
-#endif
-
-#ifdef CONFIG_IMXRT_FLEXCAN
-  ret = imxrt_can_setup();
-  if (ret < 0)
-    {
-      syslog(LOG_ERR, "ERROR: imxrt_can_setup() failed: %d\n", ret);
-      return ret;
-    }
-#endif
-
-#ifdef CONFIG_DEV_GPIO
-  /* Initialize the GPIO driver */
-
-  ret = imxrt_gpio_initialize();
-  if (ret < 0)
-    {
-      syslog(LOG_ERR, "Failed to initialize GPIO Driver: %d\n", ret);
-      return ret;
-    }
-#endif
-
-#ifdef CONFIG_PL2303
-  usbdev_serialinitialize(0);
 #endif
 
   UNUSED(ret);
