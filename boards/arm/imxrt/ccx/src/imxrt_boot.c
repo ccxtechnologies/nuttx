72cafe6fb9ae2311881d459ae01ee9b1f290ea27
Vadim Yastrebov <vadim@ccxtechnologies.com>
2021-06-14 11:40:05 -0400

    ccx: refactor memory configuration code for booting from SD Card

--- boards/arm/imxrt/ccx/src/imxrt_boot.c
+++ boards/arm/imxrt/ccx/src/imxrt_boot.c
@@ -1,36 +1,20 @@
 /****************************************************************************
  * boards/arm/imxrt/ccx/src/imxrt_boot.c
  *
- *   Copyright (C) 2015-2018 Gregory Nutt. All rights reserved.
- *   Authors: Gregory Nutt <gnutt@nuttx.org>
- *            David Sidrane <david_s5@nscdg.com>
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name NuttX nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  *
  ****************************************************************************/
 
@@ -46,7 +30,13 @@
 #include "imxrt_start.h"
 #include "ccx.h"
 #include "arm_arch.h"
-#include "imxrt_boot_mem_config.h"
+
+#ifdef CONFIG_IMXRT_BOOT_COPYTORAM
+#include "imxrt_copytoram_boot.h"
+#else
+#include "imxrt_flexspi_nor_boot.h"
+#endif
+
 
 /****************************************************************************
  * Public Functions
@@ -80,18 +70,26 @@ void imxrt_ocram_initialize(void)
   regval = getreg32(IMXRT_IOMUXC_GPR_GPR16);
   putreg32(regval | GPR_GPR16_FLEXRAM_BANK_CFG_SELF, IMXRT_IOMUXC_GPR_GPR16);
 
-  for (src = (uint32_t *)(LOCATE_IN_SRC(g_boot_data.start) + g_boot_data.size),
-       dest = (uint32_t *)(g_boot_data.start + g_boot_data.size);
-       dest < (uint32_t *) &_etext;)
+  src = (uint32_t *) (LOCATE_IN_SRC(g_boot_data.start) + g_boot_data.size);
+  dest = (uint32_t *) (g_boot_data.start + g_boot_data.size);
+
+  while (dest < (uint32_t *) &_etext)
     {
       *dest++ = *src++;
     }
 }
 
+/****************************************************************************
+ * Name: imxrt_configure_flexram
+ *
+ * Description:
+ *   Called off reset vector to reconfigure the flexRAM
+ *
+ ****************************************************************************/
 /*
 * ITCM = 0
 * DTCM = 448K (2000:0000-2006:ffff)
-* FlexRAM OCRAM = 64K (2028:0000-2028:ffff)  
+* FlexRAM OCRAM = 64K (2028:0000-2028:ffff)
 * OCRAM2 = 512K (2020:0000-2027:ffff)
 * */
 void imxrt_configure_flexram(void)
@@ -128,10 +126,11 @@ void imxrt_boardinitialize(void)
  * Description:
  *   If CONFIG_BOARD_LATE_INITIALIZE is selected, then an additional
  *   initialization call will be performed in the boot-up sequence to a
- *   function called board_late_initialize().  board_late_initialize() will be
- *   called immediately after up_intitialize() is called and just before the
- *   initial application is started.  This additional initialization phase
- *   may be used, for example, to initialize board-specific device drivers.
+ *   function called board_late_initialize().  board_late_initialize() will
+ *   be called immediately after up_intitialize() is called and just before
+ *   the initial application is started.  This additional initialization
+ *   phase may be used, for example, to initialize board-specific device
+ *   drivers.
  *
  ****************************************************************************/
 
